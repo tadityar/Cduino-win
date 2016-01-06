a35a10bf9003cc8e4cec57cab02ba1828249cc35
Sharkfinned <the27thhalo@gmail.com>
2016-01-06 13:43:20 +0800

    Update lab2_b_due.ino

--- Communicate with UDOO/eHealth/lab2_b_due/lab2_b_due.ino
+++ Communicate with UDOO/eHealth/lab2_b_due/lab2_b_due.ino
@@ -1,5 +1,3 @@
-const int ledPin = 13;
-
 int pulse = 0;
 uint8_t bpm = 0;
 
@@ -12,28 +10,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly: 
-  /*if (Serial.available())
-  {
-    bpm = Serial1.read();
-    Serial.print(bpm);
-  }
-  if (Serial.available())
-  {
-    int incomingBytes = Serial.read();
-    switch (incomingBytes)
-    {
-      case '1': 
-        digitalWrite(ledPin, HIGH);
-        Serial1.print('1');
-        break;
-      case '2':
-        digitalWrite(ledPin, LOW);
-        Serial1.print('2');
-        break;
-    }
-  }*/
   Serial.print((char)Serial1.read());
-  delay(100);
-  
+  delay(490);
 }
